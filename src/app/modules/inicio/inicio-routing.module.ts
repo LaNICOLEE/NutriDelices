dee30f85e5a76dd27cb7fea656876ea15c4df0a5
laNICOLEE <nicksva15@gmail.com>
2024-08-29 16:15:46 -0300

    recetas funciona

--- src/app/modules/inicio/inicio-routing.module.ts
+++ src/app/modules/inicio/inicio-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { CardsComponent } from './cards/cards.component';
+import { RecetasComponent } from 'src/app/pages/recetas/recetas.component';
 //Agregamos las rutas de inicio
 const routes: Routes = [
-  {path: "", component : CardsComponent},
-  {path:'inicio', component : InicioComponent}
+  {path: "cards", component : CardsComponent},
+  {path:'inicio', component : InicioComponent},
+  {path:'recetas', component : RecetasComponent}
 
 ];
 
