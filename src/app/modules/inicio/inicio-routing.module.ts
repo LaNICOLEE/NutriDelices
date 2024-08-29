6d9a711a27ac78f9408c1bfa27bf1775bff032dc
laNICOLEE <nicksva15@gmail.com>
2024-08-29 16:43:36 -0300

    actualice

--- src/app/modules/inicio/inicio-routing.module.ts
+++ src/app/modules/inicio/inicio-routing.module.ts
@@ -4,11 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { CardsComponent } from './cards/cards.component';
 import { RecetasComponent } from 'src/app/pages/recetas/recetas.component';
+import { DesayunosComponent } from 'src/app/pages/recetas/desayunos/desayunos.component';
+import { AlmuerzosComponent } from 'src/app/pages/recetas/almuerzos/almuerzos.component';
+import { MeriendasComponent } from 'src/app/pages/recetas/meriendas/meriendas.component';
+import { CenasComponent } from 'src/app/pages/recetas/cenas/cenas.component';
 //Agregamos las rutas de inicio
 const routes: Routes = [
   {path: "cards", component : CardsComponent},
   {path:'inicio', component : InicioComponent},
-  {path:'recetas', component : RecetasComponent}
+  {path:'recetas', component : RecetasComponent},
+  {path:'desayunos', component : DesayunosComponent},
+  {path:'almuerzos', component : AlmuerzosComponent},
+  {path:'meriendas', component : MeriendasComponent},
+  {path:'cenas', component : CenasComponent}
 
 ];
 
